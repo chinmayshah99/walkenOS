ab7f9584cc1df792d3a17e64a8ef1b3c8c4a7485
ads1991 <37243092+ads1991@users.noreply.github.com>
2018-04-10 15:28:40 +0530

    Add files via upload
    Menu file

---
+++ pro.cpp
@@ -0,0 +1,77 @@
+/*
+
+file manager
+
+play audio
+
+logout
+
+power off
+
+*/
+
+#include <iostream>
+#include <cstdlib>
+#include<conio.h>
+
+using namespace std;
+
+ void mainmenu()
+ {
+
+     char c;
+
+ 		cout<<"\n\n\n\n\n\t\t\t\t\t\t\t\tMenu\n\n\t\t\t\t\t\tPress the number to enter the program\n\n\n\n";
+
+ 		for (int i = 0; i < 60; ++i)
+ 		{
+ 			cout<<" ";
+ 		}
+ 		cout<<"1.File Manager\n";
+ 		for (int i = 0; i < 60; ++i)
+ 		{
+ 			cout<<" ";
+ 		}
+ 		cout<<"2.Audio Player\n";
+ 		for (int i = 0; i < 60; ++i)
+ 		{
+ 			cout<<" ";
+ 		}
+ 		cout<<"3.Logout\n";
+ 		for (int i = 0; i < 60; ++i)
+ 		{
+ 			cout<<" ";
+ 		}
+ 		cout<<"4.Power Off\n";
+
+
+        cin>>c;
+        switch(c)
+        {
+        	case '1':
+        	break;
+        	case '2':
+        	break;
+        	case '3':
+        	   system("cls");
+
+        	break;
+        	case '4':
+        		return;
+        	break;
+
+
+        }
+
+
+
+
+ }
+
+ int main()
+ {
+
+
+ 	mainmenu();
+ 	return 0;
+ }
