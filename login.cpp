07f436c57098384dd7e3acc3c3ea1825aa49f9e0
dhruvshilotri <37692214+dhruvshilotri@users.noreply.github.com>
2018-04-22 22:34:29 +0530

    Final Login Code

--- login.cpp
+++ login.cpp
@@ -6,7 +6,7 @@ using namespace std;
 
 void user()
 {
-    fstream f,f1;
+    fstream f,f1,f2;
     int ch;
     string user,pass;
     cout<<"Enter 1.Set Up New Account, 2.Login"<<endl;
@@ -16,20 +16,20 @@ void user()
         set:
         string u;
         int j=0;
-        cout<<"Enter the Username"<<endl;
+        cout<<endl<<"Set Up : "<<endl<<endl<<"Enter your User name"<<endl;
         cin>>user;
         cout<<"Enter your password"<<endl;
         cin>>pass;
         f.open("User.txt",ios::app);
         if(!f)
         {
-            cout<<"File could not open2"<<endl;
+            cout<<"File could not open"<<endl;
             return;
         }
         f1.open("User.txt",ios::in);
         if(!f1)
         {
-            cout<<"File could not open1"<<endl;
+            cout<<"File could not open"<<endl;
             return;
         }
         else
@@ -42,7 +42,7 @@ void user()
                 {
                     if(u==user)
                     {
-                        cout<<"Username Already exists.Enter another Username."<<endl;
+                        cout<<"User name Already exists. Enter another User name."<<endl;
                         f1.close();
                         f.close();
                         goto set;
@@ -51,47 +51,58 @@ void user()
             }
             f1.close();
             f<<user<<endl<<pass<<endl<<endl;
-            cout<<"Set up completed"<<endl;
+            cout<<"Set up completed."<<endl;
+            goto log;
         }
-
         f.close();
-        return;
     }
     else if(ch==2)
     {
+        log:
         string u,p;
         int i=0,flag=0;
-        cout<<"Enter Your Username"<<endl;
+        cout<<endl<<"Log In : "<<endl<<endl<<"Enter Your User name"<<endl;
         cin>>user;
         cout<<"Enter your password"<<endl;
         cin>>pass;
-        f.open("User.txt",ios::in);
-        if(!f)
+        f2.open("User.txt",ios::in);
+        if(!f2)
         {
             cout<<"File could not open"<<endl;
             return;
         }
-        while(!f.eof())
+        while(!f2.eof())
         {
             i++;
-            getline(f,u);
+            getline(f2,u);
             if(i%3==1)
             {
                 if(u==user)
                 {
                     flag=1;
-                    getline(f,p);
+                    getline(f2,p);
                     i++;
                     if(p==pass)
-                        cout<<"Login Successful!!"<<endl;
+                    {
+                        cout<<"Login Successful!!"<<endl<<"Your User name : "<<user<<endl;
+                    }
                     else
-                        cout<<"Wrong Password!"<<endl;
+                    {
+                        cout<<"Wrong Password!"<<endl<<"Enter your information again"<<endl;
+                        f2.close();
+                        goto log;
+                    }
                     break;
                 }
             }
         }
         if(flag==0)
-            cout<<"Username not found"<<endl;
+        {
+            cout<<"User name not found"<<endl;
+            f2.close();
+            goto log;
+        }
+        f2.close();
         return;
     }
 }
