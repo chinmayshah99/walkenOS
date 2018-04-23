860a5450511ea8030125ba820f9bf856e2db7ecb
walkeknow <kewalshah077@gmail.com>
2018-04-23 15:36:16 +0530

    Add main menu to main code

--- walkenOS.cpp
+++ walkenOS.cpp
@@ -1,10 +1,17 @@
+//uncomment startup
+//hello user change back to 4s
+
+
+
 #include <iostream>
 #include <chrono>
 #include <thread>
 #include <fstream>
 using namespace std;
 void startup();
-std::string user();
+void user();
+void submenu(int procno[],std::string proc[]);
+int mainmenu();
 
 int getInt() 
 {
@@ -26,14 +33,11 @@ int getInt()
 }
 
 int main() {
-    std::string username;
-    startup();
-    username = user();
-    std::cout << std::string(100,'\n');
-    std::cout << std::string(11,'\t') << "Welcome "<< username << "!\n"; //full
-    std::cout << std::string(22,'\n');
-    std::this_thread::sleep_for (std::chrono::seconds(4));
-    std::cout << std::string(100,'\n');
+    //std::string username;
+    //startup();
+    
+    user();
+    
     return 0;
 }
 
@@ -63,7 +67,7 @@ void startup() {
     system("play -q daemon_process/vista.wav");
 }
 
-std::string user()
+void user()
 {   
     system("clear");
     std::cout << std::string(10,'\n');
@@ -73,7 +77,7 @@ std::string user()
     std::string user,pass;
     std::cout<<"Enter\n\n";
     std::cout << std::string(11,'\t');
-    std::cout << "1.Set Up New Account\n"<< std::string(11,'\t') <<"2.Login"<<std::endl;
+    std::cout << "1.Set Up New Account\n"<< std::string(11,'\t') <<"2.Login"<<std::endl << std::string(11,'\t') <<"3.Power Off"<<std::endl;
     std::cout << std::string(11,'\t');
     ch = getInt();
     if(ch==1)
@@ -98,13 +102,13 @@ std::string user()
         if(!f)
         {
             std::cout<<"File could not open1"<<std::endl;
-            return NULL;
+            return;
         }
         f1.open("User.txt",ios::in);
         if(!f1)
         {
             std::cout<<"File could not open"<<std::endl;
-            return NULL;
+            return;
         }
         else
         {
@@ -157,7 +161,7 @@ std::string user()
         if(!f2)
         {
             std::cout<<"File could not open"<<std::endl;
-            return NULL;
+            return;
         }
         while(!f2.eof())
         {
@@ -175,7 +179,14 @@ std::string user()
                         std::cout << std::string(11,'\t'); //full
                         std::cout<<"Login Successful!!"<<std::endl;
                         std::this_thread::sleep_for (std::chrono::seconds(2));
-                        return user;
+                        std::cout << std::string(100,'\n');
+                        std::cout << std::string(11,'\t') << "Welcome "<< user << "!\n"; //full
+                        std::cout << std::string(22,'\n');
+                        std::this_thread::sleep_for (std::chrono::seconds(1));
+                        std::cout << std::string(100,'\n');
+    
+                        mainmenu();
+                        return;
                     }
                     else
                     {   
@@ -200,6 +211,7 @@ std::string user()
             while(1) {
                 std::cout<<"Menu:\n" << std::string(11,'\t')<<"1.Set up a new account \n"<< std::string(11,'\t') <<"2.Login with another user name\n"<<std::endl;
                 int c;
+                std::cout << std::string(11,'\t'); //full
                 c = getInt();
                 f2.close();
                 if(c==1)
@@ -216,10 +228,198 @@ std::string user()
                 }
         }
         f2.close();
-        return user;
+        return;
+    }else if(ch==3){
+        system("clear");
+        system("exit");
+        return;
     }
 }
 
+int mainmenu()
+{
+    std::string proc[]={"File manager","Audio Player","Calculator","System Monitor","Text Editor"};
+    int procno[]={0,0,0,0,0};
+	while (1) {
+		abc:
+		int c;
+		system("clear");
+		std::cout<<std::string(10,'\n')<<std::string(12, '\t')<<"Menu\n\n"<<std::string(10,'\t');
+		std::cout<<"Press the number to enter the program"<<std::string(4,'\n');
+		std::cout << std::string(11,'\t');
+
+		std::cout<<"1. File Manager\n";
+		std::cout << std::string(11,'\t');
+		std::cout<<"2. Audio Player\n";
+		std::cout << std::string(11,'\t');
+		std::cout<<"3. Calculator\n";
+		std::cout << std::string(11,'\t');
+		std::cout<<"4. Text Editor\n";
+		std::cout << std::string(11,'\t');
+		std::cout<<"5. System Monitor\n";
+		std::cout << std::string(11,'\t');
+        std::cout<<"6. Logout\n";
+		std::cout << std::string(11,'\t');
+		std::cout<<"7. Power Off\n";
+		std::cout << std::string(2,'\n')<<std::string(11,'\t')<<"Input:";
+
+		while(true) {
+			std::cin >>c;
+			std::cin.ignore(32767, '\n');
+			if(std::cin.fail()) {
+				std::cin.clear();
+				std::cin.ignore(32767, '\n');
+				pqr:
+				std::cout << "\n\n" << std::string(10,'\t') <<"Not a valid input! Re-enter key";
+				std::cout << std::endl;
+				std::this_thread::sleep_for (std::chrono::seconds(1));
+				system("clear");
+				goto abc;
+			}
+			else break;
+		}
+
+		switch(c)
+		{
+			case 1:
+				system("clear");
+			    procno[0]=1;
+			break;
+			case 2:
+			    procno[1]=1;
+                system("cmus");
+                system("clear");
+			break;
+			case 3:
+			    procno[2]=1;
+                system("clear");
+                system("gcalccmd");
+                system("clear");
+			break;
+			case 6:
+                user();
+                return 1;
+			break;
+			case 5:
+                procno[3]=1;
+                system("clear");
+                submenu(procno,proc);
+				return 1;
+			break;
+			case 4:
+				system("clear");
+				procno[4]=1;
+                system("clear");
+                system("vim");
+                system("clear");
+			break;
+			case 7:
+				system("clear");
+                system("exit");
+			return 0;
+			break;
+			default:
+                goto pqr;
+			break;
+		}
+	}
+}
+
+
+void submenu(int procno[],std::string proc[])
+{
+  while (1) {
+		abc:
+		int d;
+		system("clear");
+		std::cout<<std::string(10,'\n')<<std::string(12, '\t')<<"System Monitor\n\n"<<std::string(10,'\t');
+		std::cout<<"Press the number to enter the option"<<std::string(4,'\n');
+		std::cout << std::string(11,'\t');
+
+		std::cout<<"1. Process\n";
+		std::cout << std::string(11,'\t');
+		std::cout<<"2. Memory\n";
+		std::cout << std::string(11,'\t');
+		
+		std::cout<<"3. Back\n";
+		std::cout << std::string(11,'\t');
+		std::cout << std::string(2,'\n')<<std::string(11,'\t')<<"Input:";
+
+
+		while(true) {
+			std::cin >>d;
+			std::cin.ignore(32767, '\n');
+			if(std::cin.fail()) {
+				std::cin.clear();
+				std::cin.ignore(32767, '\n');
+				pqr:
+				std::cout << "\n\n" << std::string(10,'\t') <<"Not a valid input! Re-enter key";
+				std::cout << std::endl;
+				std::this_thread::sleep_for (std::chrono::seconds(1));
+				system("clear");
+				goto abc;
+			}
+			else break;
+		}
+    int num;
+		switch(d)
+		{
+			case 1:
+			    xyz:
+			    system("clear");
+			    std::cout << std::string(10,'\n')<<std::string(11,'\t')<<"Running Process";
+			     num=0;
+			     for(int i=0;i<5;i++)
+			     {
+			       if(procno[i]==1)
+			       {
+			         std::cout << std::string(2,'\n')<<std::string(11,'\t')<<++num<<". "<<proc[i];
+			       }
+			     }
+			     std::cout<<std::string(3,'\n')<<std::string(11, '\t')<<"Press 0 to go back \n\n";
+    		   std::cout << std::string(3,'\n')<<std::string(11,'\t')<<"Input:";
+    		  while(true) {
+    			std::cin >>d;
+    			std::cin.ignore(32767, '\n');
+    			if(std::cin.fail()) {
+    				std::cin.clear();
+    				std::cin.ignore(32767, '\n');
+    				lmn:
+    				std::cout << "\n\n" << std::string(10,'\t') <<"Not a valid input! Re-enter key";
+    				std::cout << std::endl;
+    				std::this_thread::sleep_for (std::chrono::seconds(1));
+    				system("clear");
+    				goto xyz;
+
+    			}
+    			else break;
+    		}
+    		if(d==0)
+    		{
+    		  break;
+    		}
+    		else{
+    		  goto lmn;
+    		}
+
+			case 2:
+                std::cout << std::string(2,'\n');
+                std::cout << std::string(11,'\t'); //full
+          		system("free -m");
+                std::this_thread::sleep_for (std::chrono::seconds(10));
+			break;
+			case 3:
+			    system("clear");
+			    mainmenu();
+				return;
+			break;
+
+			default:
+                goto pqr;
+			break;
+		}
+	}
 
 
 
+}
